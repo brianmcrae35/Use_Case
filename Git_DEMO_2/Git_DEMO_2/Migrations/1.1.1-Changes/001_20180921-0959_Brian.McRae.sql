5572485c68bcaaa1f10f4a29c9b3c49c15cc4687
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-09-21 10:01:41 -0700

    Remove Comments Column from AddressType

---
+++ Git_DEMO_2/Git_DEMO_2/Migrations/1.1.1-Changes/001_20180921-0959_Brian.McRae.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="a7a9e7cb-8b78-45d1-ae41-d6e54f71f252" />
+GO
+
+PRINT N'Altering [Person].[AddressType]'
+GO
+ALTER TABLE [Person].[AddressType] DROP
+COLUMN [Comments]
+GO
--- Git_DEMO_2/Git_DEMO_2/Schema-Model/Person/Tables/AddressType.sql
+++ Git_DEMO_2/Git_DEMO_2/Schema-Model/Person/Tables/AddressType.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [Person].[AddressType]
 (
 [AddressTypeID] [int] NOT NULL IDENTITY(1, 1),
-[Name] [varchar] (250) NOT NULL,
-[Comments] [varchar] (100) NULL
+[Name] [varchar] (250) NOT NULL
 )
 GO
 ALTER TABLE [Person].[AddressType] ADD CONSTRAINT [PK_AddressType_ID] PRIMARY KEY CLUSTERED  ([AddressTypeID])
